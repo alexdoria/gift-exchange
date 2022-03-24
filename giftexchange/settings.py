24570f1ead2636ebe3264ef0458fb38c92657aa0
Alex <sr.alexdoria@gmail.com>
2022-03-24 10:46:30 -0600

    Added .gitignore

--- giftexchange/settings.py
+++ giftexchange/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from i import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6bf-tbhq$brkw7n8t8%u61fw#8@xeb@--cv^tnymir*9d%u4_j'
+SECRET_KEY = KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
