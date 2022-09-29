b82e615f4feae97b548622cf38c488c774396adf
Alex Doria <sr.alexdoria@gmail.com>
2022-09-29 14:39:34 -0500

    Fix notification mail settings

--- giftexchange/settings.py
+++ giftexchange/settings.py
@@ -148,3 +148,5 @@ EMAIL_HOST = 'smtp.zoho.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
